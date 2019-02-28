f90cab8af8381cb58fcbbb6ef82242dbd5dcff35
Maria Pascu <mariapascu0498@gmail.com>
2019-02-28 14:43:58 +0200

    pizza problem

---
+++ cmake-build-debug/CMakeFiles/problemaaa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problemaaa.dir/main.cpp.obj"
+  "problemaaa.pdb"
+  "problemaaa.exe"
+  "problemaaa.exe.manifest"
+  "libproblemaaa.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problemaaa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
